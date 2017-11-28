1ca06638370b60a2e8a2db6a6b807adcd0fc1e23
popstk <ther@live.cn>
2017-11-28 18:08:36 +0800

    update

--- content.vue
+++
@@ -1,6 +0,0 @@
-<template>
-    <div class="tab-pane" :id="plugin.id">
-        <textarea id="input_text" name="input_text" class="form-control" spellcheck="false" placeholder="请输入"></textarea>
-    </div>
-</template>
-
